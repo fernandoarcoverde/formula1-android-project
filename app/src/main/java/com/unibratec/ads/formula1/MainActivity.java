9c5ded0a7bf584a003ab0d206ab4e46818626ac8
Fernando Arcoverde Cavalcanti Filho <fernandoarcoverde@hotmail.com>
2017-09-19 11:45:31 -0300

    Update MainActivity.java

--- app/src/main/java/com/unibratec/ads/formula1/MainActivity.java
+++ app/src/main/java/com/unibratec/ads/formula1/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+//import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.unibratec.ads.formula1.fragments.DetailDriverFragment;
